c1aedac76fa255b6ba4cf820e67f35fdcaff67cf
Ernesto Canquiz <caribestic123@gmail.com>
2025-10-01 20:58:06 -0400

    feat: Include service module

--- app/pages/services/index.vue
+++ app/pages/services/index.vue
@@ -8,12 +8,16 @@ servicesStore.fetchServices()
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-8 lg:grid-cols-2"> {{ servicesStore.services ? servicesStore.services : []}}
-    <!--ServiceCard 
-      v-for="service in servicesStore?.services"
-      :key="service.id"
-      :service="service"
-    /-->
+  <div class="min-h-screen bg-gray-50">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <ServiceCard 
+          v-for="service in servicesStore?.services"
+          :key="service.id"
+          :service="service"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
